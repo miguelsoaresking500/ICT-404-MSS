03956cdeb2a389e34bdb73f56c6c1631f0059601
Miguel Soares <miguel.soares@cpnv.ch>
2019-05-20 15:57:24 +0200

    update pizza

---
+++ EXO_CommandePizza/EXO_CommandePizza/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace EXO_CommandePizza
+{
+    partial class Pizza
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Table = new System.Windows.Forms.Label();
+            this.cbx_Anchois = new System.Windows.Forms.CheckBox();
+            this.txb_Table = new System.Windows.Forms.TextBox();
+            this.rdo_Xfine = new System.Windows.Forms.RadioButton();
+            this.txb_Commande = new System.Windows.Forms.TextBox();
+            this.rdo_Epaisse = new System.Windows.Forms.RadioButton();
+            this.rdo_Nomale = new System.Windows.Forms.RadioButton();
+            this.rdo_Fine = new System.Windows.Forms.RadioButton();
+            this.cbx_Câpres = new System.Windows.Forms.CheckBox();
+            this.cbx_Jambon = new System.Windows.Forms.CheckBox();
+            this.cbx_Crevettes = new System.Windows.Forms.CheckBox();
+            this.lbl_Commande = new System.Windows.Forms.Label();
+            this.btn_commander = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Table
+            // 
+            this.lbl_Table.AutoSize = true;
+            this.lbl_Table.Location = new System.Drawing.Point(59, 35);
+            this.lbl_Table.Name = "lbl_Table";
+            this.lbl_Table.Size = new System.Drawing.Size(34, 13);
+            this.lbl_Table.TabIndex = 0;
+            this.lbl_Table.Text = "Table";
+            // 
+            // cbx_Anchois
+            // 
+            this.cbx_Anchois.AutoSize = true;
+            this.cbx_Anchois.Location = new System.Drawing.Point(216, 89);
+            this.cbx_Anchois.Name = "cbx_Anchois";
+            this.cbx_Anchois.Size = new System.Drawing.Size(64, 17);
+            this.cbx_Anchois.TabIndex = 1;
+            this.cbx_Anchois.Text = "Anchois";
+            this.cbx_Anchois.UseVisualStyleBackColor = true;
+            // 
+            // txb_Table
+            // 
+            this.txb_Table.Location = new System.Drawing.Point(99, 32);
+            this.txb_Table.Name = "txb_Table";
+            this.txb_Table.Size = new System.Drawing.Size(28, 20);
+            this.txb_Table.TabIndex = 2;
+            // 
+            // rdo_Xfine
+            // 
+            this.rdo_Xfine.AutoSize = true;
+            this.rdo_Xfine.Location = new System.Drawing.Point(81, 88);
+            this.rdo_Xfine.Name = "rdo_Xfine";
+            this.rdo_Xfine.Size = new System.Drawing.Size(69, 17);
+            this.rdo_Xfine.TabIndex = 3;
+            this.rdo_Xfine.TabStop = true;
+            this.rdo_Xfine.Text = "Extra-fine";
+            this.rdo_Xfine.UseVisualStyleBackColor = true;
+            // 
+            // txb_Commande
+            // 
+            this.txb_Commande.Location = new System.Drawing.Point(39, 255);
+            this.txb_Commande.Name = "txb_Commande";
+            this.txb_Commande.Size = new System.Drawing.Size(278, 20);
+            this.txb_Commande.TabIndex = 4;
+            // 
+            // rdo_Epaisse
+            // 
+            this.rdo_Epaisse.AutoSize = true;
+            this.rdo_Epaisse.Location = new System.Drawing.Point(81, 157);
+            this.rdo_Epaisse.Name = "rdo_Epaisse";
+            this.rdo_Epaisse.Size = new System.Drawing.Size(62, 17);
+            this.rdo_Epaisse.TabIndex = 5;
+            this.rdo_Epaisse.TabStop = true;
+            this.rdo_Epaisse.Text = "Epaisse";
+            this.rdo_Epaisse.UseVisualStyleBackColor = true;
+            // 
+            // rdo_Nomale
+            // 
+            this.rdo_Nomale.AutoSize = true;
+            this.rdo_Nomale.Location = new System.Drawing.Point(81, 134);
+            this.rdo_Nomale.Name = "rdo_Nomale";
+            this.rdo_Nomale.Size = new System.Drawing.Size(64, 17);
+            this.rdo_Nomale.TabIndex = 6;
+            this.rdo_Nomale.TabStop = true;
+            this.rdo_Nomale.Text = "Normale";
+            this.rdo_Nomale.UseVisualStyleBackColor = true;
+            // 
+            // rdo_Fine
+            // 
+            this.rdo_Fine.AutoSize = true;
+            this.rdo_Fine.Location = new System.Drawing.Point(81, 111);
+            this.rdo_Fine.Name = "rdo_Fine";
+            this.rdo_Fine.Size = new System.Drawing.Size(45, 17);
+            this.rdo_Fine.TabIndex = 7;
+            this.rdo_Fine.TabStop = true;
+            this.rdo_Fine.Text = "Fine";
+            this.rdo_Fine.UseVisualStyleBackColor = true;
+            // 
+            // cbx_Câpres
+            // 
+            this.cbx_Câpres.AutoSize = true;
+            this.cbx_Câpres.Location = new System.Drawing.Point(216, 112);
+            this.cbx_Câpres.Name = "cbx_Câpres";
+            this.cbx_Câpres.Size = new System.Drawing.Size(59, 17);
+            this.cbx_Câpres.TabIndex = 8;
+            this.cbx_Câpres.Text = "Câpres";
+            this.cbx_Câpres.UseVisualStyleBackColor = true;
+            // 
+            // cbx_Jambon
+            // 
+            this.cbx_Jambon.AutoSize = true;
+            this.cbx_Jambon.Location = new System.Drawing.Point(216, 135);
+            this.cbx_Jambon.Name = "cbx_Jambon";
+            this.cbx_Jambon.Size = new System.Drawing.Size(63, 17);
+            this.cbx_Jambon.TabIndex = 9;
+            this.cbx_Jambon.Text = "Jambon";
+            this.cbx_Jambon.UseVisualStyleBackColor = true;
+            // 
+            // cbx_Crevettes
+            // 
+            this.cbx_Crevettes.AutoSize = true;
+            this.cbx_Crevettes.Location = new System.Drawing.Point(216, 158);
+            this.cbx_Crevettes.Name = "cbx_Crevettes";
+            this.cbx_Crevettes.Size = new System.Drawing.Size(71, 17);
+            this.cbx_Crevettes.TabIndex = 10;
+            this.cbx_Crevettes.Text = "Crevettes";
+            this.cbx_Crevettes.UseVisualStyleBackColor = true;
+            this.cbx_Crevettes.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // lbl_Commande
+            // 
+            this.lbl_Commande.AutoSize = true;
+            this.lbl_Commande.Location = new System.Drawing.Point(66, 214);
+            this.lbl_Commande.Name = "lbl_Commande";
+            this.lbl_Commande.Size = new System.Drawing.Size(60, 13);
+            this.lbl_Commande.TabIndex = 11;
+            this.lbl_Commande.Text = "Commande";
+            // 
+            // btn_commander
+            // 
+            this.btn_commander.Location = new System.Drawing.Point(197, 209);
+            this.btn_commander.Name = "btn_commander";
+            this.btn_commander.Size = new System.Drawing.Size(110, 23);
+            this.btn_commander.TabIndex = 12;
+            this.btn_commander.Text = "commander";
+            this.btn_commander.UseVisualStyleBackColor = true;
+            // 
+            // Pizza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 303);
+            this.Controls.Add(this.btn_commander);
+            this.Controls.Add(this.lbl_Commande);
+            this.Controls.Add(this.cbx_Crevettes);
+            this.Controls.Add(this.cbx_Jambon);
+            this.Controls.Add(this.cbx_Câpres);
+            this.Controls.Add(this.rdo_Fine);
+            this.Controls.Add(this.rdo_Nomale);
+            this.Controls.Add(this.rdo_Epaisse);
+            this.Controls.Add(this.txb_Commande);
+            this.Controls.Add(this.rdo_Xfine);
+            this.Controls.Add(this.txb_Table);
+            this.Controls.Add(this.cbx_Anchois);
+            this.Controls.Add(this.lbl_Table);
+            this.Name = "Pizza";
+            this.Text = "Pizza";
+            this.Load += new System.EventHandler(this.Pizza_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Table;
+        private System.Windows.Forms.CheckBox cbx_Anchois;
+        private System.Windows.Forms.TextBox txb_Table;
+        private System.Windows.Forms.RadioButton rdo_Xfine;
+        private System.Windows.Forms.TextBox txb_Commande;
+        private System.Windows.Forms.RadioButton rdo_Epaisse;
+        private System.Windows.Forms.RadioButton rdo_Nomale;
+        private System.Windows.Forms.RadioButton rdo_Fine;
+        private System.Windows.Forms.CheckBox cbx_Câpres;
+        private System.Windows.Forms.CheckBox cbx_Jambon;
+        private System.Windows.Forms.CheckBox cbx_Crevettes;
+        private System.Windows.Forms.Label lbl_Commande;
+        private System.Windows.Forms.Button btn_commander;
+    }
+}
+
