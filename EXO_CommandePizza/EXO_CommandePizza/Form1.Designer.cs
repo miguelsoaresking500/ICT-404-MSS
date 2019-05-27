1e9f0043d1379c492cfe96ab6be54a6101963e16
Miguel Soares <miguel.soares@cpnv.ch>
2019-05-27 15:58:39 +0200

    update

--- EXO_CommandePizza/EXO_CommandePizza/Form1.Designer.cs
+++ EXO_CommandePizza/EXO_CommandePizza/Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.cbx_Anchois = new System.Windows.Forms.CheckBox();
             this.txb_Table = new System.Windows.Forms.TextBox();
             this.rdo_Xfine = new System.Windows.Forms.RadioButton();
-            this.txb_Commande = new System.Windows.Forms.TextBox();
             this.rdo_Epaisse = new System.Windows.Forms.RadioButton();
             this.rdo_Nomale = new System.Windows.Forms.RadioButton();
             this.rdo_Fine = new System.Windows.Forms.RadioButton();
@@ -41,6 +40,9 @@
             this.cbx_Crevettes = new System.Windows.Forms.CheckBox();
             this.lbl_Commande = new System.Windows.Forms.Label();
             this.btn_commander = new System.Windows.Forms.Button();
+            this.Pâte = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pâte.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbl_Table
@@ -61,6 +63,7 @@
             this.cbx_Anchois.TabIndex = 1;
             this.cbx_Anchois.Text = "Anchois";
             this.cbx_Anchois.UseVisualStyleBackColor = true;
+            this.cbx_Anchois.CheckedChanged += new System.EventHandler(this.cbx_Anchois_CheckedChanged);
             // 
             // txb_Table
             // 
@@ -72,25 +75,19 @@
             // rdo_Xfine
             // 
             this.rdo_Xfine.AutoSize = true;
-            this.rdo_Xfine.Location = new System.Drawing.Point(81, 88);
+            this.rdo_Xfine.Location = new System.Drawing.Point(22, 14);
             this.rdo_Xfine.Name = "rdo_Xfine";
             this.rdo_Xfine.Size = new System.Drawing.Size(69, 17);
             this.rdo_Xfine.TabIndex = 3;
             this.rdo_Xfine.TabStop = true;
             this.rdo_Xfine.Text = "Extra-fine";
             this.rdo_Xfine.UseVisualStyleBackColor = true;
-            // 
-            // txb_Commande
-            // 
-            this.txb_Commande.Location = new System.Drawing.Point(39, 255);
-            this.txb_Commande.Name = "txb_Commande";
-            this.txb_Commande.Size = new System.Drawing.Size(278, 20);
-            this.txb_Commande.TabIndex = 4;
+            this.rdo_Xfine.CheckedChanged += new System.EventHandler(this.rdo_Xfine_CheckedChanged);
             // 
             // rdo_Epaisse
             // 
             this.rdo_Epaisse.AutoSize = true;
-            this.rdo_Epaisse.Location = new System.Drawing.Point(81, 157);
+            this.rdo_Epaisse.Location = new System.Drawing.Point(22, 83);
             this.rdo_Epaisse.Name = "rdo_Epaisse";
             this.rdo_Epaisse.Size = new System.Drawing.Size(62, 17);
             this.rdo_Epaisse.TabIndex = 5;
@@ -101,18 +98,19 @@
             // rdo_Nomale
             // 
             this.rdo_Nomale.AutoSize = true;
-            this.rdo_Nomale.Location = new System.Drawing.Point(81, 134);
+            this.rdo_Nomale.Location = new System.Drawing.Point(22, 60);
             this.rdo_Nomale.Name = "rdo_Nomale";
             this.rdo_Nomale.Size = new System.Drawing.Size(64, 17);
             this.rdo_Nomale.TabIndex = 6;
             this.rdo_Nomale.TabStop = true;
             this.rdo_Nomale.Text = "Normale";
             this.rdo_Nomale.UseVisualStyleBackColor = true;
+            this.rdo_Nomale.CheckedChanged += new System.EventHandler(this.rdo_Nomale_CheckedChanged);
             // 
             // rdo_Fine
             // 
             this.rdo_Fine.AutoSize = true;
-            this.rdo_Fine.Location = new System.Drawing.Point(81, 111);
+            this.rdo_Fine.Location = new System.Drawing.Point(22, 37);
             this.rdo_Fine.Name = "rdo_Fine";
             this.rdo_Fine.Size = new System.Drawing.Size(45, 17);
             this.rdo_Fine.TabIndex = 7;
@@ -129,6 +127,7 @@
             this.cbx_Câpres.TabIndex = 8;
             this.cbx_Câpres.Text = "Câpres";
             this.cbx_Câpres.UseVisualStyleBackColor = true;
+            this.cbx_Câpres.CheckedChanged += new System.EventHandler(this.cbx_Câpres_CheckedChanged);
             // 
             // cbx_Jambon
             // 
@@ -139,6 +138,7 @@
             this.cbx_Jambon.TabIndex = 9;
             this.cbx_Jambon.Text = "Jambon";
             this.cbx_Jambon.UseVisualStyleBackColor = true;
+            this.cbx_Jambon.CheckedChanged += new System.EventHandler(this.cbx_Jambon_CheckedChanged);
             // 
             // cbx_Crevettes
             // 
@@ -154,7 +154,7 @@
             // lbl_Commande
             // 
             this.lbl_Commande.AutoSize = true;
-            this.lbl_Commande.Location = new System.Drawing.Point(66, 214);
+            this.lbl_Commande.Location = new System.Drawing.Point(86, 204);
             this.lbl_Commande.Name = "lbl_Commande";
             this.lbl_Commande.Size = new System.Drawing.Size(60, 13);
             this.lbl_Commande.TabIndex = 11;
@@ -162,7 +162,7 @@
             // 
             // btn_commander
             // 
-            this.btn_commander.Location = new System.Drawing.Point(197, 209);
+            this.btn_commander.Location = new System.Drawing.Point(202, 199);
             this.btn_commander.Name = "btn_commander";
             this.btn_commander.Size = new System.Drawing.Size(110, 23);
             this.btn_commander.TabIndex = 12;
@@ -170,27 +170,52 @@
             this.btn_commander.UseVisualStyleBackColor = true;
             this.btn_commander.Click += new System.EventHandler(this.btn_commander_Click);
             // 
+            // Pâte
+            // 
+            this.Pâte.Controls.Add(this.rdo_Xfine);
+            this.Pâte.Controls.Add(this.rdo_Fine);
+            this.Pâte.Controls.Add(this.rdo_Nomale);
+            this.Pâte.Controls.Add(this.rdo_Epaisse);
+            this.Pâte.Location = new System.Drawing.Point(62, 75);
+            this.Pâte.Name = "Pâte";
+            this.Pâte.Size = new System.Drawing.Size(122, 110);
+            this.Pâte.TabIndex = 13;
+            this.Pâte.TabStop = false;
+            this.Pâte.Text = "Pâte";
+            this.Pâte.Enter += new System.EventHandler(this.Pâte_Enter);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(31, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 59);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Pizza
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(367, 303);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pâte);
             this.Controls.Add(this.btn_commander);
             this.Controls.Add(this.lbl_Commande);
             this.Controls.Add(this.cbx_Crevettes);
             this.Controls.Add(this.cbx_Jambon);
             this.Controls.Add(this.cbx_Câpres);
-            this.Controls.Add(this.rdo_Fine);
-            this.Controls.Add(this.rdo_Nomale);
-            this.Controls.Add(this.rdo_Epaisse);
-            this.Controls.Add(this.txb_Commande);
-            this.Controls.Add(this.rdo_Xfine);
             this.Controls.Add(this.txb_Table);
             this.Controls.Add(this.cbx_Anchois);
             this.Controls.Add(this.lbl_Table);
             this.Name = "Pizza";
             this.Text = "Pizza";
             this.Load += new System.EventHandler(this.Pizza_Load);
+            this.Pâte.ResumeLayout(false);
+            this.Pâte.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,7 +227,6 @@
         private System.Windows.Forms.CheckBox cbx_Anchois;
         private System.Windows.Forms.TextBox txb_Table;
         private System.Windows.Forms.RadioButton rdo_Xfine;
-        private System.Windows.Forms.TextBox txb_Commande;
         private System.Windows.Forms.RadioButton rdo_Epaisse;
         private System.Windows.Forms.RadioButton rdo_Nomale;
         private System.Windows.Forms.RadioButton rdo_Fine;
@@ -211,6 +235,8 @@
         private System.Windows.Forms.CheckBox cbx_Crevettes;
         private System.Windows.Forms.Label lbl_Commande;
         private System.Windows.Forms.Button btn_commander;
+        private System.Windows.Forms.GroupBox Pâte;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- EXO_CommandePizza/EXO_CommandePizza/Form1.cs
+++ EXO_CommandePizza/EXO_CommandePizza/Form1.cs
@@ -24,30 +24,125 @@ namespace EXO_CommandePizza
 
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (cbx_Crevettes.Checked == true) { cbx_Jambon.Checked = false; }
         }
 
         private void btn_commander_Click(object sender, EventArgs e)
         {
             int Table;
-            string Total;
-            if(txb_Table.Text==" ")
+
+            string Pate;
+            string Anchois;
+            string Câpres;
+            string Jambon;
+            string Crevettes;
+
+
+            if (txb_Table.Text == " ")
             {
                 MessageBox.Show("Introduisez une table s'il vous plaît");
             }
-            else if(int.TryParse(txb_Table.Text, out Table)==false)
+            else if (int.TryParse(txb_Table.Text, out Table) == false)
             {
                 MessageBox.Show("Introduisez une table Valable");
             }
             else
             {
-            String.Total = String.Format("Pour la {0} ", Table);
-            if(rdo_Xfine.Checked == true)
+                String Total = String.Format("Pour la {0} ", Table);
+                if (rdo_Xfine.Checked == true)
                 {
-                    Total = String.Format("Pour la{0}: {1}", Table, rdo_Xfine);
+                    Total += "Pâte Extra Fine";
+                }
+                if (rdo_Fine.Checked == true)
+                {
+                    Total += "Pâte Fine";
+                }
+                if (rdo_Nomale.Checked == true)
+                {
+                    Total += "Pâte Normale";
+                }
+                if (rdo_Epaisse.Checked == true)
+                {
+                    Total += "Pâte Epaisse";
+                }
+                if (cbx_Anchois.Checked == true)
+                {
+                    Total += "Pâte Anchois, ";
+                }
+                else
+                {
+                    Total += "";
+
+                }
+                if (cbx_Câpres.Checked == true)
+                {
+                    Total += " Câpres, ";
+                }
+                else
+                {
+                    Total += "";
+                }
+                if (cbx_Jambon.Checked == true)
+                {
+                    Total += " Jambon, ";
+                }
+                else
+                {
+                    Total += "";
+                }
+                if (cbx_Crevettes.Checked == true)
+                {
+                    Total += " Crevettes  ";
+                }
+                else
+                {
+                    Total += "";
                 }
 
+                label1.Text = Total;
+
+
             }
+        }
+
+        private void rdo_Xfine_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdo_Nomale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pâte_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbx_Anchois_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbx_Câpres_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbx_Jambon_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbx_Jambon.Checked == true) { cbx_Crevettes.Checked = false; }
+        }
+
+        private void txb_Commande_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
